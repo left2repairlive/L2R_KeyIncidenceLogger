04e10ccdea75e7f150868b4d944295dddef185a7
leo-nightfever <christ@nightfever.team>
2025-03-08 16:49:13 +0200

    Outsourced csv and png in extra functions

--- KeyIncidinceLogger.py
+++ KeyIncidinceLogger.py
@@ -3,6 +3,7 @@ import csv
 import datetime
 
 
+
 # intro
 print("==========================")
 print("== Key Incidence Logger ==")
@@ -13,35 +14,50 @@ print("Press ESC three times in a row to exit!")
 print("")
 
 
+
 # global vars
 keylog = dict()
 escCount = 0
 timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 
-# Exit the program safely and save the data
+
+# function to exit program after saving results
 def safe_exit():
     # print keylog results in terminal
     print("Results:")
     print(keylog)
     print("")
 
+    write_csv()
+
+    write_png()
+
+    quit()
+
+
+
+# function for exporting csv
+def write_csv():
     print("Saving results as .csv ...")
-    with open(timestamp + '.csv', 'w', newline='') as csvfile:
+    with open(timestamp + '.csv', 'w+', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for key in keylog:
             writer.writerow([key, keylog[key]])
     print("DONE")
     print("")
 
+
+
+# function for exporting png
+def write_png():
     print("Saving results as keymap .png ...")
     print("NYI")
     print("")
 
-    quit()
 
 
-# What happens on a single keypress
+# function for how to process a keystroke
 def on_press(key):
     global escCount
     global keylog
@@ -57,6 +73,7 @@ def on_press(key):
 
     currentIncidince = keylog.get(key, 0)
     keylog[key] = currentIncidince + 1
+
 
 
 # add key listener
